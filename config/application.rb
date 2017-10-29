51f48a276a342d4921e698599c82a9849e35bbd6
Thadeu Esteves Jr <tadeuu@gmail.com>
2017-10-29 18:22:07 -0300

    Part1 conclused.

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,14 @@ module FacebookChat
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.generators do |g|
+      # g.template_engine nil #to skip views
+      g.test_framework  nil #to skip test framework
+      # g.assets  false
+      # g.helper false
+      # g.stylesheets false
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
